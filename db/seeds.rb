3597014734f7e1f1d53058cb4c4aba99fd8b478a
Fergus Buscall <buscallf@gmail.com>
2024-11-13 14:37:40 +0000

    Index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code
+  )
+end
+puts "Finished. Created #{Restaurant.count} restaurants."
